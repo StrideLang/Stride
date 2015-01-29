b0cc383a302bdc9086d9654bb879152bd1b05afb
Pacifist79 <jtilbian@gmail.com>
2015-01-29 10:39:54 -0800

    Removed "lexar.txt" and added new stream test scripts

--- tests/tst_parsertest.cpp
+++ tests/tst_parsertest.cpp
@@ -20,8 +20,17 @@ ParserTest::ParserTest()
 
 void ParserTest::testCase1()
 {
-    parse("../../StreamStack/tests/data/lexar.txt");
-    QVERIFY2(true, "Failure");
+    int error;
+    QString path = "../../StreamStack/tests/data/";
+    QStringList files;
+    files << "simple.stream" << "array.stream" << " list.stream"
+          << "introBlock.stream"
+          << "introConverter.stream" << "introFeedback.stream"
+          << "introGenerator.stream";
+    foreach (QString file, files) {
+        error = parse(QString(path + file).toStdString().c_str());
+        QVERIFY2(error == 0, "Failure");
+    }
 }
 
 QTEST_APPLESS_MAIN(ParserTest)
