a2c5774c796480f161b8bf29308cefa60940caaf
Andres Cabrera <mantaraya36@gmail.com>
2015-03-19 22:01:11 -0700

    Actual code generation from code in json files

--- platforms/Gamma/1.0/scripts/build.py
+++ platforms/Gamma/1.0/scripts/build.py
@@ -34,7 +34,7 @@ def write_section(sec_name, code):
     f = open(filename, 'r')
     text = f.read()
     f.close()
-    log(text)
+#    log(text)
     start_index = text.find("//[[%s]]"%sec_name)
     end_index = text.find("//[[/%s]]"%sec_name, start_index)
     if start_index <0 or end_index < 0:
@@ -56,39 +56,97 @@ tree = json.load(jsonfile)
 
 stream_index = 0;
 
+init_code = ''
 dsp_code = ''
 num_chnls = 2;
+platform_funcs = json.load(open(platform_dir + '/functions.json'))['functions']
+platform_objs = json.load(open(platform_dir + '/objects.json'))['objects']
+# --------------------- Common platform functions
+
+def find_function(platform_funcs, name):
+    for func in platform_funcs:
+        if func['functionName'] == name:
+            return func
+
+def find_builtin_object(platform_objs, name):
+    for obj in platform_objs:
+        if obj['objectName'] == name:
+            return obj
 
 def get_function_code(function_name, properties, token):
-    print(properties)
-    init_code = '';
-    dsp_code = '';
-    if function_name == 'level':
-        dsp_code = token + ' = ' + token + ' * ' + str(properties['gain']['value']) + ';\n'
+    code = {}
+    new_init_code = '';
+    new_dsp_code = '';
 
-    return init_code, dsp_code
+    obj = find_function(platform_funcs, function_name)
+    if not obj:
+        raise ValueError("Function not found.")
 
+    new_init_code = obj["code"]["init_code"]["code"]
+    new_init_code = new_init_code.replace("%%token%%", var_name)
+
+    new_dsp_code = obj["code"]["dsp_code"]["code"]
+    new_dsp_code = new_dsp_code.replace("%%token%%", var_name)
+
+    for prop_name, prop_value in properties.iteritems():
+        template = '%%property:' + prop_name + '%%'
+        if template in new_init_code:
+            new_init_code = new_init_code.replace(template, str(prop_value["value"]))
+        if template in new_dsp_code:
+            new_dsp_code = new_dsp_code.replace(template, str(prop_value["value"]))
+
+    code["dsp_code"] = new_dsp_code
+    code["init_code"] = new_init_code
+    return code
+
+def get_builtin_obj_code(obj_name, var_name):
+    code = {}
+    new_init_code = '';
+    new_dsp_code = '';
+
+    obj = find_builtin_object(platform_objs, obj_name)
+    if not obj:
+        raise ValueError("Builtin onject not found.")
+
+    new_init_code = obj["code"]["init_code"]["code"]
+    new_init_code = new_init_code.replace("%%token%%", var_name)
+    new_init_code = new_init_code.replace("%%bundle_index%%", str(parts["index"] - 1))
+
+    new_dsp_code = obj["code"]["dsp_code"]["code"]
+    new_dsp_code = new_dsp_code.replace("%%token%%", var_name)
+    new_dsp_code = new_dsp_code.replace("%%bundle_index%%", str(parts["index"] - 1))
+
+    code["dsp_code"] = new_dsp_code
+    code["init_code"] = new_init_code
+    return code
+
+# ---------------------
 for node in tree:
     if 'stream' in node:
         for parts in node['stream']:
             if parts['type'] == 'Bundle':
-                if parts['name'] == 'AudioIn':
-                    chan_index = parts['index'] - 1;
-                    dsp_code += 'float sig_%02i = io.in(%i);\n'%(stream_index, chan_index)
-
-                elif parts['name'] == 'AudioOut':
-                    chan_index = parts['index'] - 1;
-                    dsp_code += 'io.out(%i) = sig_%02i;\n'%(chan_index, stream_index)
+                var_name = "sig_%02i"%(stream_index)
+                new_code = get_builtin_obj_code(parts["name"], var_name)
+                dsp_code += new_code["dsp_code"]
+                init_code += new_code["init_code"]
             elif parts['type'] == 'Name':
-                if parts['name'] == 'AudioIn':
-                    for chan_index in range(num_chnls):
-                        dsp_code += 'float sig_%02i_%02i = io.in(%i);\n'%(stream_index, chan_index, chan_index)
-                elif parts['name'] == 'AudioIn':
-                    for chan_index in range(num_chnls):
-                        dsp_code += 'io.out(%i) = sig_%02i_%02i;\n'%(chan_index, stream_index, chan_index)
+                pass
+#                if parts['name'] == 'AudioIn':
+#                    for chan_index in range(num_chnls):
+#                        dsp_code += 'float sig_%02i_%02i = io.in(%i);\n'%(stream_index, chan_index, chan_index)
+#                elif parts['name'] == 'AudioIn':
+#                    for chan_index in range(num_chnls):
+#                        dsp_code += 'io.out(%i) = sig_%02i_%02i;\n'%(chan_index, stream_index, chan_index)
             elif parts['type'] == 'Function':
-                new_init_code, new_dsp_code = get_function_code(parts['name'], parts["properties"], 'sig_%02i'%stream_index)
-                dsp_code += new_dsp_code
+                func = find_function(platform_funcs, parts["name"])
+                if not func:
+                    raise ValueError("Function not found")
+                var_name = "sig_%02i"%(stream_index)
+
+                new_code = get_function_code(parts['name'], parts["properties"], 'sig_%02i'%stream_index)
+
+                dsp_code += new_code["dsp_code"]
+                init_code += new_code["init_code"]
         stream_index += 1
 
 #log(dsp_code)
