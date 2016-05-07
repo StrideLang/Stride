9a839416bb377547aa0fa59c2b7089aa2ff1f515
Andres Cabrera <mantaraya36@gmail.com>
2016-05-06 18:25:58 -0700

    Error text now includes source file name.

--- parser/langerror.cpp
+++ parser/langerror.cpp
@@ -8,42 +8,42 @@ LangError::LangError()
 }
 
 std::string LangError::getErrorText() {
-    std::string errorText = "In file " + filename  + ":" ;
+    std::string errorText = "In file " + filename  + ":\n" ;
     switch(type) {
     case Syntax:
-        errorText = "Syntax Error: Unexpected character '" + errorTokens[0] + "'";
+        errorText += "Syntax Error: Unexpected character '" + errorTokens[0] + "'";
         break;
     case UnknownType:
-        errorText = "Unknown Type Error. Type '" + errorTokens[0] + "' not recognized.";
+        errorText += "Unknown Type Error. Type '" + errorTokens[0] + "' not recognized.";
         break;
     case InvalidType:
-        errorText = "Invalid Type Error";
+        errorText += "Invalid Type Error";
         break;
     case InvalidPort:
-        errorText = "Invalid port Error";
+        errorText += "Invalid port Error";
         break;
     case InvalidPortType:
-        errorText = "Invalid port type Error. Port '" + errorTokens[0]
-                + "' in Block '" + errorTokens[1]
+        errorText += "Invalid port type Error. Port '" + errorTokens[1]
+                + "' in Block '" + errorTokens[0]
                 + "' expects '" + errorTokens[2] + "'";
         break;
     case IndexMustBeInteger:
-        errorText = "Index to array must be integer ";
+        errorText += "Index to array must be integer ";
         break;
     case BundleSizeMismatch:
-        errorText = "Bundle Size Mismatch Error";
+        errorText += "Bundle Size Mismatch Error";
         break;
     case ArrayIndexOutOfRange:
-        errorText = "Array Index out of Range Error";
+        errorText += "Array Index out of Range Error";
         break;
     case DuplicateSymbol:
-        errorText = "Duplicate Symbol Error";
+        errorText += "Duplicate Symbol Error";
         break;
     case InconsistentList:
-        errorText = "Inconsistent List Error";
+        errorText += "Inconsistent List Error";
         break;
     case UndeclaredSymbol:
-        errorText = "Undeclared Symbol '" + errorTokens[0] + "'";
+        errorText += "Undeclared Symbol '" + errorTokens[0] + "'";
         break;
     case None:
     default:
