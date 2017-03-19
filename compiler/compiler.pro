1eb8ff9292201fa766cf057e8bbd626cbf00091c
Andres Cabrera <mantaraya36@gmail.com>
2017-03-18 22:10:41 -0700

    Initial working version of standalone command-line compiler

--- compiler/compiler.pro
+++ compiler/compiler.pro
@@ -11,6 +11,10 @@ TEMPLATE = app
 
 SOURCES += main.cpp
 
+
+INCLUDEPATH += $$PWD/../parser
+DEPENDPATH += $$PWD/../parser
+
 win32-msvc2015:CONFIG(release, debug|release): LIBS += -L$$OUT_PWD/../codegen/release/ -lcodegen
 else:win32-msvc2015:CONFIG(debug, debug|release): LIBS += -L$$OUT_PWD/../codegen/debug/ -lcodegen
 else:unix: LIBS += -L$$OUT_PWD/../codegen/ -lcodegen
@@ -21,3 +25,16 @@ DEPENDPATH += $$PWD/../codegen
 win32-msvc2015:CONFIG(release, debug|release): PRE_TARGETDEPS += $$OUT_PWD/../codegen/release/codegen.lib
 else:win32-msvc2015:CONFIG(debug, debug|release): PRE_TARGETDEPS += $$OUT_PWD/../codegen/debug/codegen.lib
 else:unix: PRE_TARGETDEPS += $$OUT_PWD/../codegen/libcodegen.a
+
+
+# Link to parser library
+win32-msvc2015:CONFIG(release, debug|release): LIBS += -L$$OUT_PWD/../parser/release/ -lStrideParser
+else:win32-msvc2015:CONFIG(debug, debug|release): LIBS += -L$$OUT_PWD/../parser/debug/ -lStrideParser
+else:unix: LIBS += -L$$OUT_PWD/../parser/ -lStrideParser
+
+INCLUDEPATH += $$PWD/../parser
+DEPENDPATH += $$PWD/../parser
+
+win32-msvc2015:CONFIG(release, debug|release): PRE_TARGETDEPS += $$OUT_PWD/../parser/release/StrideParser.lib
+else:win32-msvc2015:CONFIG(debug, debug|release): PRE_TARGETDEPS += $$OUT_PWD/../parser/debug/StrideParser.lib
+else:unix: PRE_TARGETDEPS += $$OUT_PWD/../parser/libStrideParser.a
--- compiler/main.cpp
+++ compiler/main.cpp
@@ -1,8 +1,82 @@
 #include <QCoreApplication>
+#include <QCommandLineParser>
+#include <QDebug>
+#include <QFile>
+#include <QFileInfo>
+#include <QDir>
+
+//#include "ast.h"
+#include "codevalidator.h"
+#include "pythonproject.h"
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    QCoreApplication app(argc, argv);
+    QCoreApplication::setApplicationName("stridecc");
+    QCoreApplication::setApplicationVersion("0.1-alpha");
 
-    return a.exec();
+    QCommandLineParser parser;
+    parser.setApplicationDescription("Stride command line compiler");
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addPositionalArgument("source", QCoreApplication::translate("main", "Source file to build."));
+//    parser.addPositionalArgument("destination", QCoreApplication::translate("main", "Destination directory."));
+
+    QCommandLineOption targetDirectoryOption(QStringList() << "p" << "platform-root",
+                                             QCoreApplication::translate("main", "Path to Stride Platforms directory"),
+                                             QCoreApplication::translate("main", "directory"));
+    parser.addOption(targetDirectoryOption);
+    parser.process(app);
+
+    const QStringList args = parser.positionalArguments();
+    QString platformRootPath = parser.value(targetDirectoryOption);
+    QString fileName = args.at(0);
+
+    qDebug() << args.at(0);
+    qDebug() << platformRootPath;
+
+    AST *tree;
+    tree = AST::parseFile(fileName.toLocal8Bit().constData());
+
+    if (tree) {
+        CodeValidator validator(platformRootPath, tree);
+        validator.validate();
+        //            QStringList m_types = validator.getPlatform()->getPlatformTypeNames();
+        //            m_funcs = validator.getPlatform()->getFunctionNames();
+        //            QList<AST *> objects = validator.getPlatform()->getBuiltinObjects();
+        //            m_objectNames.clear();
+        //            foreach (AST *platObject, objects) {
+        //                if (platObject->getNodeType() == AST::Block) {
+        //                    m_objectNames << QString::fromStdString(static_cast<BlockNode *>(platObject)->getName());
+        //                }
+        //            }
+        //            m_errors = validator.getErrors();
+        StridePlatform *platform = validator.getPlatform();
+
+        QFileInfo info(fileName);
+        QString dirName = info.absolutePath() + QDir::separator()
+                + info.fileName() + "_Products";
+        if (!QFile::exists(dirName)) {
+            if (!QDir().mkpath(dirName)) {
+                qDebug() << "Error creating project path";
+                return -1;
+            }
+        }
+        Builder * builder = platform->createBuilder(dirName);
+
+        builder->build(tree);
+        qDebug() << "Built in directory:" << dirName;
+
+        //            if(m_lastValidTree) {
+        //                m_lastValidTree->deleteChildren();
+        //                delete m_lastValidTree;
+        //            }
+        //            m_lastValidTree = tree;
+        delete tree;
+    } else {
+        qDebug() << "Syntax error";
+
+    }
+
+    return 0;
 }
