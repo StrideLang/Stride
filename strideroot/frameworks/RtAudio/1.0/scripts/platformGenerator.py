d055eea816e4effcd1b1b9ab78707ed2a52448ca
Joseph Tilbian <jtilbian@gmail.com>
2017-10-09 16:12:58 -0700

    Changed SDK to 10.13 for macOS

--- strideroot/frameworks/RtAudio/1.0/scripts/platformGenerator.py
+++ strideroot/frameworks/RtAudio/1.0/scripts/platformGenerator.py
@@ -372,7 +372,7 @@ class Generator(GeneratorBase):
                     "-O3",
                     "-std=c++11",
                     "-DNDEBUG",
-                    "-isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.12.sdk",
+                    "-isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.13.sdk",
                     "-Wl,-search_paths_first",
                     "-Wl,-headerpad_max_install_names"]
 
@@ -421,5 +421,3 @@ class Generator(GeneratorBase):
 #        outs, errs = self.run_process.communicate()
 #        self.log(outs)
 #        self.log(errs)
-
-
