884ffb39a2ddb7a085eee1f505f06364cfea195e
Andres Cabrera <mantaraya36@gmail.com>
2018-10-24 21:39:56 -0700

    Improvements to AsyncDomain and template file for RtAudio platform

--- strideroot/frameworks/RtAudio/1.0/project/template.cpp
+++ strideroot/frameworks/RtAudio/1.0/project/template.cpp
@@ -1,10 +1,4 @@
 
-#include "RtAudio.h"
-#include <iostream>
-#include <cstdlib>
-#include <cstring>
-
-
 //[[Includes]]
 //[[/Includes]]
 
@@ -17,17 +11,6 @@
 
 
 //[[Processing]]
-
-int inout( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
-           double streamTime, RtAudioStreamStatus status, void *data )
-{
-  // Since the number of input and output channels is equal, we can do
-  // a simple buffer copy operation here.
-  if ( status ) std::cout << "Stream over/underflow detected." << std::endl;
-  unsigned long *bytes = (unsigned long *) data;
-  memcpy( outputBuffer, inputBuffer, *bytes );
-  return 0;
-}
 //[[/Processing]]
 
 //[[OSC:Processing]]
@@ -39,17 +22,12 @@ int inout( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
 
 
 int main() {
+// std::cout << "app started" << std::endl;
 //[[Initialization]]
-
 //[[/Initialization]]
-
-// char input;
-// std::cout << "\nRunning ... press <enter> to quit.\n";
-// std::cin.get(input);
 
 //[[Cleanup]]
 //[[/Cleanup]]
-
 
  return 0;
 }
