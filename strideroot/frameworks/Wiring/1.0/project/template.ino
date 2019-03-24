b1ca64b216a10244aeaab97312ab2e07a55abb68
Andres Cabrera <mantaraya36@gmail.com>
2019-03-23 22:47:47 -0700

    Fixes for Wiring platform. Added domainProcessing port to Domain. The domainProcessing port contains code to tick the domain (if applicable).

--- strideroot/frameworks/Wiring/1.0/project/template.ino
+++ strideroot/frameworks/Wiring/1.0/project/template.ino
@@ -1,38 +1,21 @@
 
-#include "Timer.h"
 //[[Includes]]
 
 //[[/Includes]]
 
-Timer t;
 //[[Declarations]]
-
-int led = 13;
-float phase = 0.0;
-
 //[[/Declarations]]
-
-void process()
-{
-//[[Processing]]
-      Serial.print("2 second tick: millis()=");
-      Serial.println(millis());
-//[[/Processing]]
-}
 
 
 void setup() {
-
-int tickEvent = t.every(1, process);
 //[[Initialization]]
-
-  Serial.begin(115200);
-  pinMode(led, OUTPUT);
 //[[/Initialization]]
 }
 
+
 void loop() {
-  t.update();
+//[[Processing]]
+//[[/Processing]]
 
 //[[Cleanup]]
 //[[/Cleanup]]
