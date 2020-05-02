6270d66f6f977eae00aaa924e2a3ca611eb4fd07
Andres Cabrera <mantaraya36@gmail.com>
2020-05-01 18:30:26 -0700

    Commit missing TestFramework

---
+++ strideroot/frameworks/TestFramework/1.0/project/config.cmake
@@ -0,0 +1,7 @@
+#set(INCLUDE_DIRS rtaudio-4.1.2)
+#
+#set(ADDITIONAL_SOURCES rtaudio-4.1.2/RtAudio.cpp)
+#
+#set(ADDITIONAL_DEFINES -D__LINUX_PULSE__)
+#
+#set(LINK_LIBRARIES pthread pulse-simple pulse)
---
+++ strideroot/frameworks/TestFramework/1.0/project/template.cpp
@@ -0,0 +1,25 @@
+
+//[[Includes]]
+//[[/Includes]]
+
+//[[Declarations]]
+//[[/Declarations]]
+
+//[[Instances]]
+//[[/Instances]]
+
+
+//[[Processing]]
+//[[/Processing]]
+
+
+int main() {
+// std::cout << "app started" << std::endl;
+//[[Initialization]]
+//[[/Initialization]]
+
+//[[Cleanup]]
+//[[/Cleanup]]
+
+ return 0;
+}
